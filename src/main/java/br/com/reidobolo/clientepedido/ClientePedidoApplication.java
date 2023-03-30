9e063ef92581226aa61c13662e34a0fe9123c5fb
Lucas Wakanda <lucascorreiaaraujo77@gmail.com>
2023-03-30 20:08:38 -0300

    testa api de teste

--- src/main/java/br/com/reidobolo/clientepedido/ClientePedidoApplication.java
+++ src/main/java/br/com/reidobolo/clientepedido/ClientePedidoApplication.java
@@ -2,9 +2,19 @@ package br.com.reidobolo.clientepedido;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class ClientePedidoApplication {
+
+	@GetMapping
+	public String getHomeTeste() {
+		return "Cliente pedido";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientePedidoApplication.class, args);
