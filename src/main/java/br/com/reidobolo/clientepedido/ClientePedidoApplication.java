1f8408c99d8dc617df56ade5e98368fcedd2ef16
Lucas Wakanda <lucascorreiaaraujo77@gmail.com>
2023-03-30 17:43:59 -0300

    inicia projeto

---
+++ src/main/java/br/com/reidobolo/clientepedido/ClientePedidoApplication.java
@@ -0,0 +1,13 @@
+package br.com.reidobolo.clientepedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientePedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientePedidoApplication.class, args);
+	}
+
+}
