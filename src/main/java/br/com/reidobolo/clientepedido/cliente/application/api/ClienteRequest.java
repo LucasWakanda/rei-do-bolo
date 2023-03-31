81f372c13679b3c39fd45f900893367aa10ef2e6
Lucas Wakanda <lucascorreiaaraujo77@gmail.com>
2023-03-30 21:33:30 -0300

    implementa request e response

--- src/main/java/br/com/reidobolo/clientepedido/cliente/application/api/ClienteRequest.java
+++ src/main/java/br/com/reidobolo/clientepedido/cliente/application/api/ClienteRequest.java
@@ -1,5 +1,35 @@
 package br.com.reidobolo.clientepedido.cliente.application.api;
 
-public class ClienteRequest {
+import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+import br.com.reidobolo.clientepedido.cliente.domain.Sexo;
+import lombok.Value;
+
+@Value
+public class ClienteRequest {
+	@NotBlank
+	private String nomeCompleto;
+	@NotBlank
+	@Email
+	@Column(unique = true)
+	private String  email;
+	@NotBlank
+	private String telefone;
+	@Enumerated(EnumType.STRING)
+	private Sexo sexo;
+	@NotNull
+	private LocalDate dataNascimento;
+	@CPF
+	@Column(unique = true)
+	private String cpf;
+	 
 }
--- src/main/java/br/com/reidobolo/clientepedido/cliente/application/api/ClienteResponse.java
+++ src/main/java/br/com/reidobolo/clientepedido/cliente/application/api/ClienteResponse.java
@@ -1,5 +1,10 @@
 package br.com.reidobolo.clientepedido.cliente.application.api;
 
+import java.util.UUID;
+
+import lombok.Value;
+@Value
 public class ClienteResponse {
+	private UUID idCliente;
 
 }
