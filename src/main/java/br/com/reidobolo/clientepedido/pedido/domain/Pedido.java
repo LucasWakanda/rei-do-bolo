7fb39bf6c3a80a1a461b7bb334503043df644d50
Lucas Wakanda <lucascorreiaaraujo77@gmail.com>
2023-04-03 16:06:41 -0300

    implementa dominio pedido

--- src/main/java/br/com/reidobolo/clientepedido/pedido/domain/Pedido.java
+++ src/main/java/br/com/reidobolo/clientepedido/pedido/domain/Pedido.java
@@ -1,5 +1,34 @@
 package br.com.reidobolo.clientepedido.pedido.domain;
 
-public class Pedido {
+import java.time.LocalDateTime;
+import java.util.UUID;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Entity
+public class Pedido {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(columnDefinition = "uuid", name = "idPedido", updatable = false, unique = true, nullable = false)
+	private UUID idPedido;
+	@Column(columnDefinition = "uuid", name = "idClientePedido", nullable = false)
+	private UUID idClientePedido;
+	@Enumerated(EnumType.STRING)
+	private Sabor sabor;
+	private int quantidade;
+	
+	private LocalDateTime dataHoraDoCadastro;
+	private LocalDateTime dataHoraDaUltimaAlteracao;
 }
---
+++ src/main/java/br/com/reidobolo/clientepedido/pedido/domain/Sabor.java
@@ -0,0 +1,5 @@
+package br.com.reidobolo.clientepedido.pedido.domain;
+
+public enum Sabor {
+	BOLODEMORANGO, BOLODEMARACUJA, BOLODECOCOFESTA, BOLODEAMENDOIN, BOLODECHOCOLATE, BOLODENUTTELA;
+}
